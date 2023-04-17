8f1d2485fce6744a286741582cd607e738d5ed63
Shammy Wammy <svassell2@brooklynsteamcenter.org>
2023-04-17 14:52:57 -0400

    Message

---
+++ hello.js
@@ -0,0 +1,26 @@
+console.log("Hello World")
+
+X = "ShamourV"
+Y = 17
+
+print("Hello my name is" + X + "My age is" + Y)
+
+if (1006==9001) {
+    console.log("Somethings wrong")
+}
+else if (9001==1006) {
+
+}
+else{
+    console.log("It does not")
+}
+
+let percentage = 100
+if (percentage <= 100 && percentage >= 80 ) {
+    grade = "A";
+}
+let myList = (DragonBall, Naruto, OnePiece)
+for (let item of myList) {
+    
+}
+
