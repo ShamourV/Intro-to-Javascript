12439e9f0478193cceb0be602512c5340e09b192
Shammy Wammy <svassell2@brooklynsteamcenter.org>
2023-05-02 14:53:24 -0400

    Stopwatch

--- stopwatch.js
+++ stopwatch.js
@@ -1,3 +1,30 @@
+
+
+let milliseconds = 0
+let seconds = 0
+let minutes = 0
+
+let digits = document.getElementById('digits')
+
+let startButton = document.getElementById('start-btn')
+
+let interval;
+
 function start () {
-    alert("You clicked the start button")
+
+    startButton.disabled = true
+
+    interval = setInterval(function () {
+    milliseconds = milliseconds +10
+
+    if (milliseconds >=1000) {
+        seconds = seconds + 1
+        milliseconds = 0
+    }
+if (seconds >= 60) {
+   minutes = minutes + 1
+    seconds = 0
+}
+digits.innerHTML = String(minutes).padStart(2, '0') + ':' + String(seconds).padStart(2, '0') + ':' + milliseconds
+}, 10)
 }
