408d1bf4f65293ad29658ab547777e914a93f8c7
Shammy Wammy <svassell2@brooklynsteamcenter.org>
2023-05-03 14:53:46 -0400

    done

--- Turtle.js
+++ Turtle.js
@@ -3,31 +3,38 @@ let aquarium = document.getElementById("aquarium")
 
 let turtle = new RealTurtle.default(aquarium, {autoStart: true})
 
-turtle.setStrokeColorRGB(200, 200, 255)
-turtle.setFillStyle('red')
+turtle.setStrokeColorRGB(0, 0, 0)
 
 turtle.setPosition(50, 100)
 
 turtle.fill()
 
-turtle.setFillStyle('red')
+turtle.setFillStyle('brown')
 
 turtle.setPosition(150, 150)
-
+turtle.setSpeed(0.9)
 turtle.beginPath()
 
-turtle.forward(50)
+turtle.forward(90)
 turtle.right(90)
-turtle.forward(50)
+turtle.forward(60)
 turtle.right(90)
-turtle.forward(50)
+turtle.forward(90)
 turtle.right(90)
-turtle.forward(50)
+turtle.forward(60)
+turtle.fill()
+turtle.closePath()
+
 turtle.right(90)
-turtle.forward(50)
+turtle.forward(90)
 turtle.right(45)
 turtle.forward(40)
-turtle.right(100)
-turtle.forward(40)
+turtle.right(85)
+turtle.forward(43)
 turtle.right(50)
-turtle.forward(1)
+
+turtle.setFillStyle()
+turtle.setPosition(250, 20)
+turtle.arc(10, 360)
+turtle.setStrokeColorRGB(255, 255, 255)
+turtle.fill()
